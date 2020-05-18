08a7b367e67719ca2857236d43c9283374f8d24e
Hakim Ghods <ghods.hakim@hotmail.com>
2020-05-18 21:38:09 +0430

    Post List

--- src/Portal.Web/Areas/User/Pages/Posts/Index.cshtml.cs
+++ src/Portal.Web/Areas/User/Pages/Posts/Index.cshtml.cs
@@ -1,16 +1,36 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Portal.Web.Areas.User.Pages.Posts
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("http://localhost:5501/api/post");
+            PostList = JsonConvert.DeserializeObject<List<PostViewModel>>
+                (await response.Content.ReadAsStringAsync());
+            response.EnsureSuccessStatusCode();
+            return Page();
+        }
+
+        public List<PostViewModel> PostList { get; set; }
+
+        public class PostViewModel
+        {
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+            public Byte[] ImageData { get; set; }
         }
     }
 }
--- src/services/Polaris.Api/Controllers/PostController.cs
+++ src/services/Polaris.Api/Controllers/PostController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Polaris.Api.Data;
 using Polaris.Api.Models;
 
@@ -23,7 +24,7 @@ namespace Polaris.Api.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<Post>> Create([FromBody]Post post)
+        public async Task<ActionResult<Post>> Create([FromBody] Post post)
         {
             post.Id = Guid.NewGuid();
             post.TimeCreated = DateTime.Now;
@@ -34,6 +35,15 @@ namespace Polaris.Api.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Post>> GetAll()
+        {
+            var post = await _db.Posts.ToListAsync();
+            return Ok(post);
+        }
+
+        [HttpGet("api/[controller]/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Post>> GetById(string id)
